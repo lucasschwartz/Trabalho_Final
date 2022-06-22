a519e674c069b4ba625bcaa58c97856e36f71815
Lavínia Winter <lavinia.winter@edu.pucrs.br>
2022-06-22 10:49:44 -0300

    a

--- Anotacoes.java
+++ Anotacoes.java
@@ -7,52 +7,22 @@ import java.util.Scanner;
  */
 public class Anotacoes
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    public int dia;
+    
     /**
      * Constructor for objects of class Anotacoes
      */
-    public Anotacoes()
+    public Anotacoes(int dia)
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        this.dia = dia;
     }
     
     /**
      *  Lugar para definir a data
      */
-    public void Data(String[] args)
+    public void Data()
     {
-        int dia, mes, ano;
-        Scanner teclado = new Scanner(System.in);
-        dia = teclado.nextInt();
-        mes = teclado.nextInt();
-        ano = teclado.nextInt();
-        teclado.close();
         
-        System.out.printf("%d,/ %d,/ %d,/ %n", dia, mes, ano);
     }
     
-    public void Hora(String[] args) {
-        int hora, min;
-        Scanner teclado = new Scanner(System.in);
-        hora = teclado.nextInt();
-        min = teclado. nextInt();
-        teclado.close();
-        
-        System.out.printf("%d, : %d, %n", hora, min);
-    }
 }
