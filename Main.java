33bed81c6872240e16baec67f728bd1e055271bd
Gabriel <zurawski.gabriel@edu.pucrs.br>
2022-06-22 10:49:16 -0300

    Menu

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class Main here.
  *
@@ -11,11 +11,12 @@ public class Main
            "Columbina picui", "Gubernetes yetapa", "Xolmis cinereus",
            "Griseotyrannus aurantioatrocristatus", "Prognet tapera", "Suiriri suiriri", 
            "Pyrocephalus runinus"};
+           
     
     /**
      * Dados ficaram salvos dentro dessa classe.
      */
-    public static void main()
+    /*public static void main()
     {
         Ave aves[];
         aves = new Ave[10];
@@ -25,6 +26,53 @@ public class Main
         
         for (int j = 0; j < 10; j++){
             System.out.printf("%s%n", aves[j].getNome());
+        }
+    }*/
+    
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        Ave aves[];
+        aves = new Ave[10];
+        int number = 1;
+        for(int i = 0; i < 10; i++) {
+            aves[i] = new Ave(nomesCientificos[i]);
+        }
+    
+        while(number != 0) {
+            number = sc.nextInt();
+            switch (number) {
+                case 1:
+                    System.out.printf("%s%n", aves[0].getNome());
+                    break;
+                case 2:
+                    System.out.printf("%s%n", aves[1].getNome());
+                    break;
+                case 3:
+                    System.out.printf("%s%n", aves[2].getNome());
+                    break;
+                case 4:
+                    System.out.printf("%s%n", aves[3].getNome());
+                    break;
+                case 5:
+                    System.out.printf("%s%n", aves[4].getNome());
+                    break;
+                case 6:
+                    System.out.printf("%s%n", aves[5].getNome());
+                    break;
+                case 7:
+                    System.out.printf("%s%n", aves[6].getNome());
+                    break;
+                case 8:
+                    System.out.printf("%s%n", aves[7].getNome());
+                    break;
+                case 9:
+                    System.out.printf("%s%n", aves[8].getNome());
+                    break;
+                case 10:
+                    System.out.printf("%s%n", aves[9].getNome());
+                    break;
+                    
+            }
         }
     }
 }
